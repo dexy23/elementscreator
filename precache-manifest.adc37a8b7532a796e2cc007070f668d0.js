aa8f2214e6bc1cef9e8285e04891cfa30127f17b
Andrii Vasylyshyn <koriolis@i.ua>
2019-10-17 11:34:55 +0300

    Updates

---
+++ precache-manifest.adc37a8b7532a796e2cc007070f668d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1aac66348f0bfa47953e6ba14ab103",
+    "url": "/elementscreator/index.html"
+  },
+  {
+    "revision": "e310d96597b886920541",
+    "url": "/elementscreator/static/css/main.83014950.chunk.css"
+  },
+  {
+    "revision": "5eb244f2deb378924e18",
+    "url": "/elementscreator/static/js/2.3aa268bd.chunk.js"
+  },
+  {
+    "revision": "e310d96597b886920541",
+    "url": "/elementscreator/static/js/main.525282f8.chunk.js"
+  },
+  {
+    "revision": "e86f9bee30e947a4759c",
+    "url": "/elementscreator/static/js/runtime-main.73d441d6.js"
+  }
+]);
